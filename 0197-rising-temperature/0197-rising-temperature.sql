89821e567c592b1256ca17ddc23ffba896219ea1
Valerie Tsai <118166488+valerietsai@users.noreply.github.com>
2026-03-17 09:56:42 +0800

    Time: 406 ms (68.11%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,19 @@
-/* Write your T-SQL query statement below */
-select
-    today.id
-from Weather as today
-    left join Weather as yesterday
-        on today.recordDate = dateadd(day, 1, yesterday.recordDate)
-where yesterday.temperature < today.temperature
+with YesterDay as (
+    select 
+        id
+        , dateadd(day, -1, recordDate) as yesterDay
+    from Weather
+),
+YesterDay_tmp as (
+    select
+        b.id
+        , b.yesterDay
+        , a.temperature
+    from Weather as a
+        left join YesterDay as b on a.recordDate=b.yesterDay
+)
+
+select a.id
+from Weather as a
+    left join YesterDay_tmp as b on a.id=b.id
+where a.temperature > b.temperature
